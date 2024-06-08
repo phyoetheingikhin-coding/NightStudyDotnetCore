5b9d5170784222ba723a29b7a725d674034a3349
PhyoeTheingi Khin <phyokhin869cu@gmail.com>
2024-06-08 10:50:55 +0630

    Add project files.

---
+++ NightStudyDotNetCore.MVCApp/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using NightStudyDotNetCore.MVCApp.Models;
+using System.Data.SqlClient;
+
+namespace NightStudyDotNetCore.MVCApp
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource=".\\MSSQLSERVER1",
+                InitialCatalog ="TestDb",
+                UserID="sa",
+                Password="phyo@123",
+                TrustServerCertificate=true
+
+            };
+            optionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<CustomerModel> Customers { get; set; }
+    }
+}
---
+++ NightStudyDotNetCore.MVCApp/Models/CustomerModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NightStudyDotNetCore.MVCApp.Models
+{
+    [Table("Tbl_Customer")]
+    public class CustomerModel
+    {
+        [Key]
+        public int CustomerId {  get; set; }
+        public string CustomerName { get; set; }
+        public string PhoneNo { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; }
+
+    }
+}
